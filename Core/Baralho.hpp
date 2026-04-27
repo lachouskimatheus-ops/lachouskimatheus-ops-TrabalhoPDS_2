33918cd06cac45289a307c3930d96d11fefe2d37
João Luís Fernandes Mendes Gomes <joaoluisfernandesmendesgomes@gmail.com>
2026-04-27 00:39:23 -0300

    Fiz a implementação da classe mãe do Baralho (óbvio que pode estar sujeita à alterações futuras) e implementei a primeira classe filha (BaralhoSujo) que vai servir tanto para o truco, quanto para o FDP

---
+++ Core/Baralho.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <random>
+
+class Carta; //Evita dor de cabeça
+
+class Baralho {
+
+protected:
+	std::vector<Carta*> cartas_;
+
+public:
+	Baralho() = default; //O default basicamente diz pro C++ ficar de boa e construir o objeto assim mesmo (já que o vetor já nasce vazio)
+	void embaralhar();
+	virtual ~Baralho();
+	Carta* puxarCarta();
+
+	virtual void inicializar() = 0; //Esse método garante que a classe mãe vai servir apenas de molde, cada classe filha de baralho vai TER que implementar seu próprio baralho específico ex: "BaralhoFDP"
+
+	//Getter
+	int getQtdCartas() const;
+
+};
---
+++ Truco/BaralhoSujo.cpp
@@ -0,0 +1,18 @@
+#include "Carta.hpp"
+#include "BaralhoSujo.hpp"
+
+BaralhoSujo::BaralhoSujo() {
+	inicializar();
+}
+
+void BaralhoSujo::inicializar(){
+	std::string naipes[4] = {"Paus", "Copas", "Espadas", "Ouros"};
+
+	for (const std::string& naipe : naipes) {
+		for(int i = 1; i <= 13; i++) {
+			if(i == 8 || i == 9 || i == 10) continue;
+			Carta* novaCarta = Carta(i, naipe);
+			cartas_.push_back(novaCarta);
+		};
+	};
+}
