873c5e0fe31f1bb075f32395a54208df555a13c9
João Luís Fernandes Mendes Gomes <joaoluisfernandesmendesgomes@gmail.com>
2026-04-28 23:12:48 -0300

    Corrigi o nome da classe BaralhoSujo

--- Truco/BaralhoSujo.hpp
+++ Truco/BaralhoSujo.hpp
@@ -1,10 +1,10 @@
 #pragma once
 #include "Baralho.hpp"
 
-class BaralhoFDP : public Baralho {
+class BaralhoSujo : public Baralho {
 public:
-	BaralhoFDP();
-	~BaralhoFDP() override = default; //O override é pra sobrescrever o método da classe mãe, basicamente garante a implementação do método certo e não de um método novo aleatório (sem querer)
+	BaralhoSujo();
+	~BaralhoSujo() override = default; //O override é pra sobrescrever o método da classe mãe, basicamente garante a implementação do método certo e não de um método novo aleatório (sem querer)
 
 	void inicializar() override;
 };
