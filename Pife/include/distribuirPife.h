9e2b8d0a67de97cb2777eab5936f328cf5a122cd
Matheus Lachouski <lachouskimatheus@gmail.com>
2026-05-06 22:02:05 -0300

    Readme para a entrega parcial

---
+++ Pife/include/distribuirPife.h
@@ -0,0 +1,9 @@
+#ifndef DISTRIBUIR_PIFE_H
+#define DISTRIBUIR_PIFE_H
+
+#include "baralho.h"
+#include "jogadorPife.h"
+
+void distribuirCartasPife(Baralho& baralho, JogadorPife& jogador, int quantidade);
+
+#endif
