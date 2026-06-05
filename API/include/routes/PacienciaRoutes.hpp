ef2c2867fea495b627049b7405bba8976b5cd14f
Matheus Lachouski <lachouskimatheus@gmail.com>
2026-06-04 23:08:06 -0300

    PacienciaRoutes

---
+++ API/include/routes/PacienciaRoutes.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "dependencias/crow_all.h"
+
+class PacienciaRoutes {
+public:
+    static void registrar(crow::SimpleApp& app);
+};
---
+++ API/src/routes/FrontedRoutes.cpp
@@ -0,0 +1,40 @@
+#include "routes/FrontendRoutes.hpp"
+
+void FrontendRoutes::registrar(crow::SimpleApp& app){
+
+    CROW_ROUTE(app, "/pages/<path>")
+    ([](const std::string& path){
+
+        crow::response res;
+        res.set_static_file_info("frontend/pages/" + path);
+
+        return res;
+    });
+
+    CROW_ROUTE(app, "/assets/<path>")
+    ([](const std::string& path){
+
+        crow::response res;
+        res.set_static_file_info("frontend/assets/" + path);
+
+        return res;
+    });
+
+    CROW_ROUTE(app, "/css/<path>")
+    ([](const std::string& path){
+
+        crow::response res;
+        res.set_static_file_info("frontend/css/" + path);
+
+        return res;
+    });
+
+    CROW_ROUTE(app, "/js/<path>")
+    ([](const std::string& path){
+
+        crow::response res;
+        res.set_static_file_info("frontend/js/" + path);
+
+        return res;
+    });
+}
