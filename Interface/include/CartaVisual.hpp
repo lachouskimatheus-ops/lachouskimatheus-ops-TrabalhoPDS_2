d1999227715bd5836266227ddd31918497f360d6
Lucas Pereira <lucasgamerbr788@mail.com>
2026-06-08 18:04:15 -0300

    Teste

---
+++ Interface/include/CartaVisual.hpp
@@ -0,0 +1,27 @@
+#ifndef CARTAVISUAL_HPP
+#define CARTAVISUAL_HPP
+
+#include <SFML/Graphics.hpp>
+#include "cartas.h"
+
+class CartaVisual {
+private:
+    Carta carta_;
+    sf::Sprite sprite_;
+    float largura_;
+    float altura_;
+public:
+    CartaVisual(const Carta& carta, const sf::Texture& textura);
+
+    void setScale(sf::Vector2f escala);
+
+    void definirPosicao(float x, float y);
+
+    void desenhar(sf::RenderWindow& janela);
+
+    bool foiClicada(sf::Vector2f mouse) const;
+
+    Carta mostrarCarta() const;
+};
+
+#endif
