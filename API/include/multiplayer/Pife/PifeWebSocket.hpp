d30d7cd1749c045f0d473d838b4628c65434d5ff
Matheus Lachouski <lachouskimatheus@gmail.com>
2026-06-09 15:01:03 -0300

    Servidor Paciencia

--- API/include/multiplayer/Pife/PifeWebSocket.hpp
+++ API/include/multiplayer/Pife/PifeWebSocket.hpp
@@ -4,69 +4,30 @@
 #include <map>
 #include <string>
 
-#include <crow_all.h>
+#include "crow_all.h"
 
 #include "coreAPI/SessaoWebSocket.hpp"
 #include "multiplayer/Pife/SalaPife.hpp"
 
 class PifeWebSocket {
 private:
-    static std::map<
-        crow::websocket::connection*,
-        SessaoWebSocket
-    > sessoes_;
+    static std::map<crow::websocket::connection*, SessaoWebSocket> sessoes_;
 
-    static void entrarNaSala(
-        crow::websocket::connection& conn,
-        const crow::json::rvalue& dados
-    );
+    static void entrarNaSala(crow::websocket::connection& conn, const crow::json::rvalue& dados);
+    static void processarAcao(crow::websocket::connection& conn, const crow::json::rvalue& dados);
+    static void processarPing(crow::websocket::connection& conn);
 
-    static void processarAcao(
-        crow::websocket::connection& conn,
-        const crow::json::rvalue& dados
-    );
+    static void enviarMensagem(crow::websocket::connection& conn, const crow::json::wvalue& mensagem);
+    static void enviarErro(crow::websocket::connection& conn, const std::string& mensagem);
+    static void enviarEstadoSala(SalaPife* sala);
+    static void enviarEstadoJogador(SalaPife* sala, int idJogador, crow::websocket::connection& conn);
 
-    static void processarPing(
-        crow::websocket::connection& conn
-    );
-
-    static void enviarMensagem(
-        crow::websocket::connection& conn,
-        const crow::json::wvalue& mensagem
-    );
-
-    static void enviarErro(
-        crow::websocket::connection& conn,
-        const std::string& mensagem
-    );
-
-    static void enviarEstadoSala(
-        SalaPife* sala
-    );
-
-    static void enviarEstadoJogador(
-        SalaPife* sala,
-        int idJogador,
-        crow::websocket::connection& conn
-    );
-
-    static void adicionarCartaAoJson(
-        crow::json::wvalue& destino,
-        const Carta& carta
-    );
-
-    static void removerConexao(
-        crow::websocket::connection& conn
-    );
-
-    static bool tokenValido(
-        const std::string& tokenReconexao
-    );
+    static void adicionarCartaAoJson(crow::json::wvalue& destino, const Carta& carta);
+    static void removerConexao(crow::websocket::connection& conn);
+    static bool tokenValido(const std::string& tokenReconexao);
 
 public:
-    static void registrar(
-        crow::SimpleApp& app
-    );
+    static void registrar(crow::SimpleApp& app);
 };
 
 #endif
