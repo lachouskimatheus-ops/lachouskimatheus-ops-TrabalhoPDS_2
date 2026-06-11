d5ff9f12a004e1fa150c41683ba1e568f050af38
Sassano23 <sassano23@hotmail.com>
2026-06-11 10:40:28 -0300

    Create test_poker.cpp

---
+++ Jogos/Poker/test/test_poker.cpp
@@ -0,0 +1,9 @@
+g++ -std=c++17 -Wall -Wextra \
+-IJogos/Poker/include \
+-IJogos/Core/include \
+Jogos/Poker/test/test_poker.cpp \
+Jogos/Poker/src/Poker.cpp \
+Jogos/Poker/src/MesaPoker.cpp \
+Jogos/Core/src/Carta.cpp \
+Jogos/Core/src/Baralho.cpp \
+-o Jogos/Poker/test/test_poker
