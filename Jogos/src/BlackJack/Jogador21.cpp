4ca5350df15e1cde5ab01e0e9ecb9c86ae8f0e32
kssio <jcassio455@gmail.com>
2026-06-11 16:36:41 -0300

    Refactor Jogador21 class methods and add new features

--- Jogos/src/BlackJack/Jogador21.cpp
+++ Jogos/src/BlackJack/Jogador21.cpp
@@ -1,19 +1,27 @@
+jogador 21.cpp
 #include "Jogador21.hpp"
-#include "Carta.hpp"
 
-Jogador21::Jogador21(int id, std::string nome) : Jogador(id, nome), pontuacaoAtual_(0) {}
+Jogador21::Jogador21(int id, const std::string& nome)
+    : Jogador(id, nome),
+      pontuacaoAtual_(0),
+      parou_(false) {
+}
 
 int Jogador21::calcularPontuacao() {
     int soma = 0;
     int quantidadeAses = 0;
 
     for (Carta* carta : mao_) {
+        if (carta == nullptr) {
+            continue;
+        }
+
         int valorCarta = carta->getValor();
 
-        if (valorCarta == 1) { 
+        if (valorCarta == 1) {
             quantidadeAses++;
             soma += 11;
-        } else if (valorCarta >= 11 && valorCarta <= 13) { 
+        } else if (valorCarta >= 11 && valorCarta <= 13) {
             soma += 10;
         } else {
             soma += valorCarta;
@@ -21,7 +29,7 @@ int Jogador21::calcularPontuacao() {
     }
 
     while (soma > 21 && quantidadeAses > 0) {
-        soma -= 10; 
+        soma -= 10;
         quantidadeAses--;
     }
 
@@ -29,10 +37,32 @@ int Jogador21::calcularPontuacao() {
     return pontuacaoAtual_;
 }
 
-bool Jogador21::estourou() const {
-    return pontuacaoAtual_ > 21;
+bool Jogador21::estourou() {
+    return calcularPontuacao() > 21;
 }
 
 int Jogador21::getPontuacao() const {
     return pontuacaoAtual_;
+}
+
+void Jogador21::parar() {
+    parou_ = true;
+}
+
+bool Jogador21::parou() const {
+    return parou_;
+}
+
+void Jogador21::prepararNovaRodada() {
+    for (Carta* carta : mao_) {
+        delete carta;
+    }
+
+    mao_.clear();
+    pontuacaoAtual_ = 0;
+    parou_ = false;
+}
+
+const std::vector<Carta*>& Jogador21::verMao() const {
+    return mao_;
 }
